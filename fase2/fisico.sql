187d9c1643e9e8cb9f7197a816b9993e094299c5
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2017-10-26 22:14:17 -0200

    Change double quotes to single quotes

--- fase2/fisico.sql
+++ fase2/fisico.sql
@@ -211,16 +211,16 @@ FOREIGN KEY(mensagem_id) REFERENCES mensagem(id)
 -- Inserção de tuplas para povoar o BD
 -----------------------------------------------------------------------
 
-INSERT INTO usuario (id, username, email, senha) VALUES (1, "ian", "ian@ime.usp.br", "123456");
-INSERT INTO usuario (id, username, email, senha) VALUES (2, "leonardo", "leonardo@ime.usp.br", "leonardo_123");
-INSERT INTO usuario (id, username, email, senha) VALUES (3, "isabella", "isabella@ime.usp.br", "isabella_123");
-INSERT INTO usuario (id, username, email, senha) VALUES (4, "bruno", "bruno@ime.usp.br", "bruno_123");
-INSERT INTO usuario (id, username, email, senha) VALUES (5, "eduardo", "eduardo@ime.usp.br", "eduardo_123");
-INSERT INTO usuario (id, username, email, senha) VALUES (6, "jef", "jef@ime.usp.br", "jef_123");
+INSERT INTO usuario (id, username, email, senha) VALUES (1, 'ian', 'ian@ime.usp.br', '123456');
+INSERT INTO usuario (id, username, email, senha) VALUES (2, 'leonardo', 'leonardo@ime.usp.br', 'leonardo_123');
+INSERT INTO usuario (id, username, email, senha) VALUES (3, 'isabella', 'isabella@ime.usp.br', 'isabella_123');
+INSERT INTO usuario (id, username, email, senha) VALUES (4, 'bruno', 'bruno@ime.usp.br', 'bruno_123');
+INSERT INTO usuario (id, username, email, senha) VALUES (5, 'eduardo', 'eduardo@ime.usp.br', 'eduardo_123');
+INSERT INTO usuario (id, username, email, senha) VALUES (6, 'jef', 'jef@ime.usp.br', 'jef_123');
 
-INSERT INTO grupo (id, nome) VALUES (1, "MAC0350");
+INSERT INTO grupo (id, nome) VALUES (1, 'MAC0350');
 
-INSERT INTO projeto VALUES (1, "EP2", "Aprofundando em SQL", current_timestamp, 1);
+INSERT INTO projeto VALUES (1, 'EP2', 'Aprofundando em SQL', current_timestamp, 1);
 
 INSERT INTO participa_de (grupo_id, projeto_id) VALUES (1, 1);
 
@@ -233,14 +233,14 @@ INSERT INTO pertence_a (user_id, grupo_id) VALUES (6, 1);
 
 INSERT INTO administra VALUES (1, 6, current_timestamp, '2022-12-25');
 
-INSERT INTO atividade VALUES (1, "Criacao Modelo fisico", "Criar o modelo fisico para a tabela", "2017-10-26", current_timestamp, 1);
-INSERT INTO atividade VALUES (2, "Queries no BD", "Realizar queries que envolvam ao menos tres relacoes no banco de dados", "2017-10-26", current_timestamp, 1);
+INSERT INTO atividade VALUES (1, 'Criacao Modelo fisico', 'Criar o modelo fisico para a tabela', '2017-10-26', current_timestamp, 1);
+INSERT INTO atividade VALUES (2, 'Queries no BD', 'Realizar queries que envolvam ao menos tres relacoes no banco de dados', '2017-10-26', current_timestamp, 1);
 
-INSERT INTO tag VALUES ("Vermelho", "Alta prioridade");
-INSERT INTO tag VALUES ("Verde", "Entregue");
+INSERT INTO tag VALUES ('Vermelho', 'Alta prioridade');
+INSERT INTO tag VALUES ('Verde', 'Entregue');
 
-INSERT INTO possui VALUES (1, "Vermelho", "Alta prioridade");
-INSERT INTO possui VALUES (2, "Verde", "Entregue");
+INSERT INTO possui VALUES (1, 'Vermelho', 'Alta prioridade');
+INSERT INTO possui VALUES (2, 'Verde', 'Entregue');
 
 INSERT INTO usuario_atividade VALUES (1, 1);
 INSERT INTO usuario_atividade VALUES (3, 1);
@@ -251,11 +251,11 @@ INSERT INTO convida VALUES (6, 3, 1, current_timestamp);
 INSERT INTO convida VALUES (6, 4, 1, current_timestamp);
 INSERT INTO convida VALUES (6, 5, 1, current_timestamp);
 
-INSERT INTO mensagem VALUES (1, "Entrega tarefa", "Ola, professor. Entreguei a atividade 1 que estava sob minha responsabilidade", current_timestamp);
-INSERT INTO mensagem VALUES (2, "Status Grupo", "Ola, jef. Acredito que vamos atrasar, precisamos melhorar a distribuicao das tarefas", current_timestamp);
-INSERT INTO mensagem VALUES (3, "Ajuda", "Jef, nao estou conseguindo avancar na tarefa pois estou tendo dificuldades em comunicar com o BD, pode me ajudar?", current_timestamp);
-INSERT INTO mensagem VALUES (4, "Tarefa", "Ola, Isa. Como esta a entrega da tarefa? Preciso dela para dar continuidade ao meu trabalho", current_timestamp);
-INSERT INTO mensagem VALUES (5, "Cobranca", "Vamos, Ian, termina essa tarefa, temos um prazo a cumprir", current_timestamp);
+INSERT INTO mensagem VALUES (1, 'Entrega tarefa', 'Ola, professor. Entreguei a atividade 1 que estava sob minha responsabilidade', current_timestamp);
+INSERT INTO mensagem VALUES (2, 'Status Grupo', 'Ola, jef. Acredito que vamos atrasar, precisamos melhorar a distribuicao das tarefas', current_timestamp);
+INSERT INTO mensagem VALUES (3, 'Ajuda', 'Jef, nao estou conseguindo avancar na tarefa pois estou tendo dificuldades em comunicar com o BD, pode me ajudar?', current_timestamp);
+INSERT INTO mensagem VALUES (4, 'Tarefa', 'Ola, Isa. Como esta a entrega da tarefa? Preciso dela para dar continuidade ao meu trabalho', current_timestamp);
+INSERT INTO mensagem VALUES (5, 'Cobranca', 'Vamos, Ian, termina essa tarefa, temos um prazo a cumprir', current_timestamp);
 
 INSERT INTO envia VALUES (1, 1);
 INSERT INTO envia VALUES (3, 2);
@@ -265,15 +265,14 @@ INSERT INTO envia VALUES (3, 5);
 
 -----------------------------------------------------------------------
 -- Consulta envolvendo, no mínimo, 3 relações:
--- Verifica nome do usuario que enviou uma mensagem e o titulo e 
--- corpo dessa mensagem
+-- Verifica nome do usuário que enviou uma mensagem, além do título e
+-- corpo dessa mensagem.
 -----------------------------------------------------------------------
 
 SELECT remetente.username, mensagem.titulo, mensagem.corpo
 FROM envia
-    INNER JOIN mensagem 
+    INNER JOIN mensagem
         ON mensagem.id = envia.mensagem_id
     INNER JOIN usuario as remetente
         ON remetente.id = envia.user_remetente
     WHERE user_remetente = 1;
-        
