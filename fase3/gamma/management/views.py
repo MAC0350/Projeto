756bb7237fa9699ce7299c4fbdb55b809d9c30c0
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2017-12-02 17:44:00 -0200

    Add simple view for users (username and email)

--- fase3/gamma/management/views.py
+++ fase3/gamma/management/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Usuario
 
 # Create your views here.
 def index(request):
     return render(request, 'management/index.html')
+
+def user(request):
+    usuarios = Usuario.objects.order_by('username')
+    return render(request, 'management/user.html', {'usuarios': usuarios})
