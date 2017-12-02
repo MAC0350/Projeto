cb4e09d7da0c81dd21005a89a6e0fc471747402d
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2017-12-02 20:14:41 -0200

    Create view to submit new users
    - Fix href links in HTML tags

--- fase3/gamma/management/urls.py
+++ fase3/gamma/management/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^index$', views.index, name='index'),
-    url(r'^user$', views.user, name='user'),
-    url(r'^group$', views.group, name='group'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^user/$', views.user, name='user'),
+    url(r'^user/new/$', views.user_edit, name='user_edit'),
+    url(r'^group/$', views.group, name='group'),
 ]
--- fase3/gamma/management/views.py
+++ fase3/gamma/management/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from .models import Usuario, Grupo, PertenceA
+from .forms import UsuarioForm
 
 # Create your views here.
 def index(request):
@@ -8,6 +10,18 @@ def index(request):
 def user(request):
     usuarios = Usuario.objects.order_by('username')
     return render(request, 'management/user.html', {'usuarios': usuarios})
+
+def user_edit(request):
+    if request.method == "POST":
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.data_horario_criacao = timezone.now()
+            user.save()
+            return redirect('../')
+    else:
+        form = UsuarioForm()
+    return render(request, 'management/user_edit.html', {'form': form})
 
 def group(request):
     groups = Grupo.objects.order_by('nome')
