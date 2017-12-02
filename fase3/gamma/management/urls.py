aca48dd5336f1494875ac8710912c2d5f3330d5d
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2017-12-02 18:59:59 -0200

    Add view for groups

--- fase3/gamma/management/urls.py
+++ fase3/gamma/management/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='home'),
     url(r'^index$', views.index, name='index'),
     url(r'^user$', views.user, name='user'),
+    url(r'^group$', views.group, name='group'),
 ]
--- fase3/gamma/management/views.py
+++ fase3/gamma/management/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Usuario
+from .models import Usuario, Grupo, PertenceA
 
 # Create your views here.
 def index(request):
@@ -8,3 +8,17 @@ def index(request):
 def user(request):
     usuarios = Usuario.objects.order_by('username')
     return render(request, 'management/user.html', {'usuarios': usuarios})
+
+def group(request):
+    groups = Grupo.objects.order_by('nome')
+
+    num_members = []
+    for group in groups:
+        count = PertenceA.objects.filter(grupo=group.id).count()
+        num_members.append(count)
+
+    names = (g.nome for g in groups)
+    times = (g.data_horario_criacao for g in groups)
+
+    name_num = zip(names, num_members, times)
+    return render(request, 'management/group.html', {'groups': name_num})
